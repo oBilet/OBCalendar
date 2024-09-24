fe77c90548f6c03ffe82fd30cb217b472f6d3b42
omerkantar <iomerkantar@gmail.com>
2024-09-24 17:56:08 +0300

    feat: obilet calendar view created.

--- Sources/OBCalendar/OBCalendar.swift
+++ Sources/OBCalendar/OBCalendar.swift
@@ -3,6 +3,48 @@
 
 import SwiftUI
 
-public struct OBCalendar {
+public struct OBCalendar<
+    Year: View,
+    Month: View,
+    Day: View
+>: View {
     
+    let years: [CalendarModel.Year]
+    
+    @ViewBuilder let yearContent: (
+        _ year: CalendarModel.Year,
+        _ scrollProxy: ScrollViewProxy?,
+        _ monthsView: OBCollectionView<Month,
+        CalendarModel.Month>
+    ) -> Year
+        
+    @ViewBuilder let monthContent: (
+        _ model: (
+            year: CalendarModel.Year,
+            month: CalendarModel.Month
+        ),
+        _ scrollProxy: (
+            year: ScrollViewProxy?,
+            month: ScrollViewProxy?
+        ),
+        _ daysView: OBCollectionView<Day, CalendarModel.Day>
+    ) -> Month
+    
+    @ViewBuilder let dayContent: (
+        _ model: (
+            year: CalendarModel.Year,
+            month: CalendarModel.Month,
+            day: CalendarModel.Day
+        ),
+        _ scrollProxy: (
+            year: ScrollViewProxy?,
+            month: ScrollViewProxy?,
+            day: ScrollViewProxy?
+        )
+    ) -> Day
+
+    
+    public var body: some View {
+        Text("")
+    }
 }
