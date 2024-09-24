4d3d10dc41e0feda25e62ae249e88cfc9a11139a
Metin Tarık Kiki <89997840+mtktsl@users.noreply.github.com>
2024-09-24 12:14:04 +0300

    feature: OBCalendar SPM Package added.

---
+++ OBCalendar/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OBCalendar",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "OBCalendar",
+            targets: ["OBCalendar"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "OBCalendar"),
+        .testTarget(
+            name: "OBCalendarTests",
+            dependencies: ["OBCalendar"]),
+    ]
+)
