3381180b19be7c493601d999dcb256e9b892d740
Metin Tarık Kiki <89997840+mtktsl@users.noreply.github.com>
2024-09-24 14:11:14 +0300

    fix: OBCalendar library name change for import syntax.

--- Package.swift
+++ Package.swift
@@ -10,15 +10,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ObiletCalendar",
-            targets: ["OBCalendar"]),
+            targets: ["ObiletCalendar"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OBCalendar"),
+            name: "ObiletCalendar"),
         .testTarget(
             name: "OBCalendarTests",
-            dependencies: ["OBCalendar"]),
+            dependencies: ["ObiletCalendar"]),
     ]
 )
--- Sources/OBCalendar/OBCalendar.swift
+++ Sources/OBCalendar/OBCalendar.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public enum OBCalendar {
+public enum CalendarTestEnum {
     public static func printTest() {
         print("TEST")
     }
